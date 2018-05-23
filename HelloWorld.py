7bc2ce4a40c2d907dc66f81b64da7d49a2d1c986
bosoncode <suda1527403057@163.com>
2018-05-23 23:02:55 +0800

    brief introduction

---
+++ HelloWorld.py
@@ -0,0 +1,30 @@
+print('Hello world')
+print("Hello world")
+print('''Hello world''')
+print("Hello \
+world")
+
+'''output:
+Hello world
+Hello world
+Hello world
+Hello world
+'''
+
+'''
+python 是面向对象的解释型语言(写一行执行一行, 不需要编译),也常被成为胶水语言, 因为其强大的库
+
+python的一些特点:
+1. 人生苦短, python因为强大的库, 所以代码通常来说是相当简短的
+2. 缩进, 这种强迫症式的规则有人喜欢也有人讨厌, 总之一定要注意缩进, 新手特别容易错
+3. 速度, python的运行速度是相当慢的(相对于C语言来说), Python的追求目标是开发的速度,
+因为强大的库简洁的代码, 所以开发速度很快
+4. python是开源的, 欢迎大家贡献
+
+python '\' 在字符串中表示衔接, 比如一行不够写了或者代码想换行写但是内容要接在之前
+那么可以 加个 \ 来表示衔接, 具体看 第四个样例
+
+python 注释可以看output以下的内容 在运行的时候都是什么都不显示的
+'''
+
+
